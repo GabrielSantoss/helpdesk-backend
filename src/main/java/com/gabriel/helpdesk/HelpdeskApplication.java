6464d8489f974b4809f2a5b90e4628a7497ddf91
Gabriel Santos <gabriel.santos@glipsolutions.com.br>
2022-03-28 12:19:31 -0300

    Carga inicial da base

--- src/main/java/com/gabriel/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/gabriel/helpdesk/HelpdeskApplication.java
@@ -1,13 +1,52 @@
 package com.gabriel.helpdesk;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class HelpdeskApplication {
+import com.gabriel.helpdesk.domain.Chamado;
+import com.gabriel.helpdesk.domain.Cliente;
+import com.gabriel.helpdesk.domain.Tecnico;
+import com.gabriel.helpdesk.domain.enuns.Perfil;
+import com.gabriel.helpdesk.domain.enuns.Prioridade;
+import com.gabriel.helpdesk.domain.enuns.Status;
+import com.gabriel.helpdesk.repositories.ChamadoRepository;
+import com.gabriel.helpdesk.repositories.ClienteRepository;
+import com.gabriel.helpdesk.repositories.TecnicoRepository;
 
+@SpringBootApplication
+public class HelpdeskApplication implements CommandLineRunner {
+
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HelpdeskApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Tecnico tec1 = new Tecnico(null, "Gabriel Santos", "67116126040", "gabriel@email.com", "123");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente(null, "Linus", "68475956041", "linus@email.com", "123");
+		
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO , "Chamado 01", "Primeiro chamado", tec1, cli1);
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+		
 	}
 
 }
