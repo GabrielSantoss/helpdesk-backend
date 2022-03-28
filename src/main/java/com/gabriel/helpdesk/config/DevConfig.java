e39eb1b755a4930ab09aea4b323a5fd12340a9fb
Gabriel Santos <gabriel.santos@glipsolutions.com.br>
2022-03-28 14:11:33 -0300

    Perfil de desenvolvimento

---
+++ src/main/java/com/gabriel/helpdesk/config/DevConfig.java
@@ -0,0 +1,28 @@
+package com.gabriel.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.gabriel.helpdesk.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbservice;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+	
+	@Bean //faz com que o perfil seja carregado automaticamente
+	public boolean instanciaDB() {
+		if (value.equals("create")) {
+			this.dbservice.instanciaDB();
+		}
+		return false;
+	}
+}
