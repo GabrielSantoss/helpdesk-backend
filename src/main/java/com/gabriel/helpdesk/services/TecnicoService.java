cf468d4b752e9bb619fca19e29db5833ddce72c3
Gabriel Santos <gabriel.santos@glipsolutions.com.br>
2022-03-31 09:48:19 -0300

    meteo PUT para Tecnico implementado

--- src/main/java/com/gabriel/helpdesk/services/TecnicoService.java
+++ src/main/java/com/gabriel/helpdesk/services/TecnicoService.java
@@ -3,6 +3,8 @@ package com.gabriel.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -38,6 +40,14 @@ public class TecnicoService {
 		Tecnico newObj = new Tecnico(objDTO);
 		return repository.save(newObj);
 	}
+	
+	public Tecnico update(Integer id, @Valid TecnicoDTO objDTO) {
+		objDTO.setId(id);
+		Tecnico oldObj = findById(id);
+		validaPorCPFEmail(objDTO);
+		oldObj = new Tecnico(objDTO);
+		return repository.save(oldObj);
+	} 
 
 	private void validaPorCPFEmail(TecnicoDTO objDTO) {
 		Optional<Pessoa> obj = pessoaRepository.findByCpf(objDTO.getCpf());
@@ -49,5 +59,7 @@ public class TecnicoService {
 	    if (obj.isPresent() && obj.get().getId() != objDTO.getId()) {
 	    	throw new DataIntegrityViolationException("Email já cadastrado no sistema");
 	    }
-	} 
+	}
+
+
 }
